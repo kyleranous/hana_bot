18f07af5e9b18ecd0545a8a992f68f1968a2696d
Kyle Ranous <kranous05@gmail.com>
2023-05-17 23:56:57 -0600

    Started Refactoring to remove scripts and comman functions from the listener

--- discord_listener.py
+++ discord_listener.py
@@ -1,21 +1,54 @@
-import discord
-import dotenv
+"""
+HANA Discord Chat Bot
+"""
 
+import os
+import logging
+import urllib.parse
+import discord
+from discord.ext import commands
+import dotenv
+from modules.github_commands import Github
+from modules.math_commands import Math
+
+
+dotenv.load_dotenv()
+
+LOGGER = logging.getLogger(__name__)
+# Setup LOGGER to print to the terminal
+LOGGER.addHandler(logging.StreamHandler())
+
+LOGGER.setLevel(logging.DEBUG)
+
+
+DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
+
+DESCRIPTION = 'HANA Discord Bot'
 intents = discord.Intents.default()
 intents.message_content = True
+intents.members = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='/', description=DESCRIPTION, intents=intents)
 
-@client.event
+
+@bot.event
 async def on_ready():
-    print(f'We have logged in as {client.user}')
+    """ 
+    Setup the bot when it is ready
+    """
+    print(f'We have logged in as {bot.user}')
+    await bot.add_cog(Github(bot))
+    await bot.add_cog(Math(bot))
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+@bot.command()
+async def url_encode(ctx, encode: str):
+    """
+    Recieves a string and returns the URL Encoded version
+    EX: /url_encode "This is a string to encode"
+    """
+    LOGGER.debug('URL Encode request recieved: %s', encode)
+    encoded_string = urllib.parse.quote(encode)
+    await ctx.send(encoded_string)
 
-client.run('your token here')
+bot.run(DISCORD_TOKEN)
