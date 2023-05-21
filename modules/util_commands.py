d19c6cc10e2178052424e6a279f59c4254d01435
Kyle Ranous <kranous05@gmail.com>
2023-05-20 20:21:03 -0600

    Added url_encode to util_commands

--- modules/util_commands.py
+++ modules/util_commands.py
@@ -1,11 +1,16 @@
 """
 Utility Class for performing various functions with HANA Bot
 """
+import logging
+import urllib.parse
 import requests
 import discord
 from discord.ext import commands
 from handlers import qr_generator
 
+
+LOGGER = logging.getLogger('discord')
+LOGGER.setLevel(logging.DEBUG)
 
 class Utils(commands.Cog):
     """
@@ -17,6 +22,7 @@ class Utils(commands.Cog):
         '''
         Take in a string, generate a QR Code and send it to the user
         '''
+        LOGGER.info('Create QR request recieved: %s From User %s', data, ctx.author.name)
         img_path = qr_generator.generate_qr_code(data)
         await ctx.reply(file=discord.File(img_path))
 
@@ -25,6 +31,7 @@ class Utils(commands.Cog):
         '''
         Take in a word and return the definition
         '''
+        LOGGER.info('Define request recieved: %s From User %s', word, ctx.author.name)
         url = f'https://api.dictionaryapi.dev/api/v2/entries/en/{word}'
 
         response = requests.request("GET", url, timeout=10)
@@ -39,6 +46,17 @@ class Utils(commands.Cog):
             await ctx.reply(msg)
 
         else:
-            msg = f"Unable to find a definition for {word}."
+            LOGGER.warning('Error fetching definition: %s', response.text)
+            msg = f"Unable to find a definition for {word}. "
             msg += "Please try again in a little bit or let Kyle know."
             await ctx.reply(msg)
+
+    @commands.command()
+    async def url_encode(self, ctx, encode: str):
+        """
+        Recieves a string and returns the URL Encoded version
+        EX: /url_encode "This is a string to encode"
+        """
+        LOGGER.debug('URL Encode request recieved: %s', encode)
+        encoded_string = urllib.parse.quote(encode)
+        await ctx.reply(encoded_string)
